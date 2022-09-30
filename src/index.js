9c7db1404d8bafe47fbfdde989913320b6af1aac
Hung That Ton <tonh@tscti.com>
2022-09-30 01:01:56 -0400

    checkin broke code

--- src/index.js
+++ src/index.js
@@ -1,32 +1,18 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-import './index.css'
-function BookList() {
+const AmazonElectronic =() => {
     return (
-        <section className='booklist'>
-           <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-        </section>
+        <div className='booklist'>
+            <p className='book'>
+                <img src="https://m.media-amazon.com/images/I/411y5UdVmvL._AC_UL480_QL65_.jpg" alt=''/>
+            </p>
+            <p className='book'>
+                <img src="https://m.media-amazon.com/images/I/6182S7MYC2L._AC_UL480_QL65_.jpg" alt=''/>
+
+            </p>
+        </div>
     );
 }
 
-const Book = () => {
-    return (
-        <article className='book'>
-            <Image></Image>
-            <Title/>
-            <Author/>
-        </article>
-    )
-}
-
-const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg" alt=''/>
-const Title = () => <h1>I Love You to the Moon and Back</h1>;
-const Author = () => <h4>Amelia Hepworth</h4>;
-
-ReactDom.render(<BookList />, document.getElementById('root'));
+ReactDom.render(</>, document.getElementById('root'));
