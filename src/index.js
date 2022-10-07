6ff3b33b65a8d85eec7054dea01a93a11720deda
Hung That Ton <tonh@tscti.com>
2022-10-07 00:21:58 -0400

    section44 useState

--- src/index.js
+++ src/index.js
@@ -1,45 +1,17 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-
+import UseStateCounter from './useState-counter'
 import './index.css'
 
-const firstBook = {
-    img: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/8144Vic9C5L._AC_UL320_.jpg',
-    title: 'I love you to the Moon and Back',
-    author: 'Amelia Hepworth',
-}
 
-const secondBook = {
-    img: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/71aLultW5EL._AC_UY218_.jpg',
-    title: 'Our Class is a Family',
-    author: 'Shannon Olsen',
-}
 
 function BookList() {
     return (
         <section className='booklist'>
-           <Book
-            img={firstBook.img}
-            title={firstBook.title}
-            author={firstBook.author}
-           />
-            <Book
-                img={secondBook.img}
-                title={secondBook.title}
-                author={secondBook.author}
-            />
+            <UseStateCounter/>
         </section>
     );
 }
 
-const Book = ({img, title, author}) => {
-    return (
-        <article className='book'>
-            <img src={img} alt='' />
-                <h1>{title}</h1>
-                <h4>{author}</h4>
-        </article>
-    )
-}
 
 ReactDom.render(<BookList />, document.getElementById('root'));
---
+++ src/useState-counter.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+const UseStateCounter = () => {
+
+    const [value, setValue] = useState(0);
+    
+    return (
+        <>
+            <section style={{ margin: '4rem 0' }}>
+                <button className='btn' onClick={() => setValue(value + 1)}>
+                    increase
+                </button><br/>
+                <h2>Count:{value}</h2>
+                <button className='btn' onClick={() => setValue(value - 1)}>
+                    decrease
+                </button><br/><br/>
+
+                <button className='btn' onClick={() => setValue(0)}>
+                    reset
+                </button>
+            </section>
+        </>
+    );
+};
+
+export default UseStateCounter;
