8f6c3dab544903e230888184be6a84a428bd027c
Hung That Ton <tonh@tscti.com>
2022-10-06 23:02:39 -0400

    section26 props

--- src/index.js
+++ src/index.js
@@ -2,31 +2,44 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 import './index.css'
+
+const firstBook = {
+    img: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/8144Vic9C5L._AC_UL320_.jpg',
+    title: 'I love you to the Moon and Back',
+    author: 'Amelia Hepworth',
+}
+
+const secondBook = {
+    img: 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/71aLultW5EL._AC_UY218_.jpg',
+    title: 'Our Class is a Family',
+    author: 'Shannon Olsen',
+}
+
 function BookList() {
     return (
         <section className='booklist'>
-           <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
+           <Book
+            img={firstBook.img}
+            title={firstBook.title}
+            author={firstBook.author}
+           />
+            <Book
+                img={secondBook.img}
+                title={secondBook.title}
+                author={secondBook.author}
+            />
         </section>
     );
 }
 
-const Book = () => {
+const Book = ({img, title, author}) => {
     return (
         <article className='book'>
-            <Image></Image>
-            <Title/>
-            <Author/>
+            <img src={img} alt='' />
+                <h1>{title}</h1>
+                <h4>{author}</h4>
         </article>
     )
 }
-
-const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg" alt=''/>
-const Title = () => <h1>I Love You to the Moon and Back</h1>;
-const Author = () => <h4>Amelia Hepworth</h4>;
 
 ReactDom.render(<BookList />, document.getElementById('root'));
